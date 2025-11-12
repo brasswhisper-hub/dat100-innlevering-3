ca24d5032edfd2d00cb82be06dd4699f7735c197
larsbirger <larsbirger.bergmaal@gmail.com>
2025-11-12 17:42:56 +0100

    Update Bilde.java

--- src/main/java/no/hvl/dat100/oppgave2/Tekst.java
+++ src/main/java/no/hvl/dat100/oppgave2/Tekst.java
@@ -1,44 +1,43 @@
 package no.hvl.dat100.oppgave2;
 
-import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave1.*;
 
 public class Tekst extends Innlegg {
 
-	private String tekst;
-	
-	public Tekst () {
-		
-	}
+	protected String tekst;
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
-		super(id, bruker, dato);
-        this.tekst = tekst;
+		
+		this(id, bruker, dato, 0, tekst);
 	}
 	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
+		
 		super(id, bruker, dato, likes);
         this.tekst = tekst;
 	}
 	
 	public String getTekst() {
+		
 		return tekst;
 	}
 
 	public void setTekst(String tekst) {
+		
 		this.tekst = tekst;
 	}
 
 	@Override
 	public String toString() {
-		return "TEKST\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "\n" + tekst + "\n";
-
+		
+		return super.toString("TEKST")
+		+ this.tekst + "\n";
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
+	@Override
 	public String toHTML() {
-			
-		throw new UnsupportedOperationException(TODO.method());
-				
+		
+		return super.toHTML("<p>" + this.tekst + "</p>");
 	}
 }
